447088746a6a92dc7ac3b770e748a5cc547c9844
vicoar <vico.ar@gmail.com>
2014-11-08 22:23:29 -0300

    add contract database

--- app/src/main/java/com/example/viko/sunshine/MainActivity.java
+++ app/src/main/java/com/example/viko/sunshine/MainActivity.java
@@ -28,6 +28,8 @@ import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
+    String LOG_TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,8 +39,38 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new ForecastFragment())
                     .commit();
         }
+        Log.i(LOG_TAG, "onCreate");
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/example/viko/sunshine/data/BaseColumns.java
@@ -0,0 +1,8 @@
+package com.example.viko.sunshine.data;
+
+/**
+ * Created by viko on 08/11/2014.
+ */
+public interface BaseColumns {
+    public static final String _ID = "_id";
+}
