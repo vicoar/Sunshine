c4430b2f09be18bed706e37af1c80b6ad7fe46f1
vicoar <vico.ar@gmail.com>
2014-11-15 12:43:06 -0300

    shorter test

--- app/src/androidTest/java/com/example/viko/sunshine/test/TestDb.java
+++ app/src/androidTest/java/com/example/viko/sunshine/test/TestDb.java
@@ -2,6 +2,7 @@ package com.example.viko.sunshine.test;
 
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
 import android.util.Log;
@@ -9,6 +10,8 @@ import android.util.Log;
 import com.example.viko.sunshine.data.WeatherContract.LocationEntry;
 import com.example.viko.sunshine.data.WeatherContract.WeatherEntry;
 import com.example.viko.sunshine.data.WeatherDbHelper;
+
+import java.util.Set;
 
 /**
  * Created by viko on 15/11/2014.
@@ -60,20 +63,16 @@ public class TestDb extends AndroidTestCase {
         );
 
         if ( cursor.moveToFirst() ) {
-            int locationIndex = cursor.getColumnIndex(LocationEntry.COLUMN_LOCATION_SETTING);
-            int nameIndex     = cursor.getColumnIndex(LocationEntry.COLUMN_CITY_NAME);
-            int latIndex      = cursor.getColumnIndex(LocationEntry.COLUMN_COORD_LAT);
-            int longIndex     = cursor.getColumnIndex(LocationEntry.COLUMN_COORD_LONG);
+            ContentValues map = new ContentValues();
+            DatabaseUtils.cursorRowToContentValues(cursor, map);
 
-            String location = cursor.getString(locationIndex);
-            String name = cursor.getString(nameIndex);
-            double latitude = cursor.getDouble(latIndex);
-            double longitude = cursor.getDouble(longIndex);
+            Set<String> keySet = values.keySet();
 
-            assertEquals(testLocation, location);
-            assertEquals(testName, name);
-            assertEquals(testLat, latitude);
-            assertEquals(testLong, longitude);
+            for (String key : keySet) {
+                int idx = cursor.getColumnIndex(key);
+                Object val = cursor.getString(idx);
+                assertEquals(values.get(key).toString(), val);
+            }
         } else {
             fail("No values returned");
         }
@@ -83,8 +82,8 @@ public class TestDb extends AndroidTestCase {
         double testDegrees = 1.1;
         double testHumidity = 1.2;
         double testPressure = 1.3;
-        double testMaxTemp = 75;
-        double testMinTemp = 65;
+        double testMaxTemp = 75.2;
+        double testMinTemp = 65.2;
         String testShortDesc = "Asteroids";
         double testWindSpeed = 5.5;
         int testWeatherId = 321;
@@ -129,38 +128,16 @@ public class TestDb extends AndroidTestCase {
         );
 
         if ( weatherCursor.moveToFirst() ) {
-            int locKeyIndex    = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_LOC_KEY);
-            int dateTextIndex  = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_DATETEXT);
-            int degreesIndex   = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_DEGREES);
-            int humidityIndex  = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_HUMIDITY);
-            int pressureIndex  = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_PRESSURE);
-            int maxTempIndex   = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_MAX_TEMP);
-            int minTempIndex   = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_MIN_TEMP);
-            int showDescIndex  = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_SHORT_DESC);
-            int windSpeedIndex = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_WIND_SPEED);
-            int weatherIdIndex = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_WEATHER_ID);
+            ContentValues weatherMap = new ContentValues();
+            DatabaseUtils.cursorRowToContentValues(weatherCursor, weatherMap);
 
-            int locKey       = weatherCursor.getInt(locKeyIndex);
-            String dateText  = weatherCursor.getString(dateTextIndex);
-            double degrees   = weatherCursor.getDouble(degreesIndex);
-            double humidity  = weatherCursor.getDouble(humidityIndex);
-            double pressure  = weatherCursor.getDouble(pressureIndex);
-            double maxTemp   = weatherCursor.getDouble(maxTempIndex);
-            double minTemp   = weatherCursor.getDouble(minTempIndex);
-            String shortDesc = weatherCursor.getString(showDescIndex);
-            double windSpeed = weatherCursor.getDouble(windSpeedIndex);
-            int weatherId    = weatherCursor.getInt(weatherIdIndex);
+            Set<String> weatherKeySet = weatherValues.keySet();
 
-            assertEquals(locationRowId, locKey);
-            assertEquals(testDateText, dateText);
-            assertEquals(testDegrees, degrees);
-            assertEquals(testHumidity, humidity);
-            assertEquals(testPressure, pressure);
-            assertEquals(testMaxTemp, maxTemp);
-            assertEquals(testMinTemp, minTemp);
-            assertEquals(testShortDesc, shortDesc);
-            assertEquals(testWindSpeed, windSpeed);
-            assertEquals(testWeatherId, weatherId);
+            for (String key : weatherKeySet) {
+                int wIdx = weatherCursor.getColumnIndex(key);
+                String wVal = weatherCursor.getString(wIdx);
+                assertEquals(weatherValues.getAsString(key), wVal);
+            }
         } else {
             fail("No values returned");
         }
